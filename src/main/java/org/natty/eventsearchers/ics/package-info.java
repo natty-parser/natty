75860096f53ddcc6acb679f064b549f4c432c510
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-09 20:34:35 +0200

    A description for this package

---
+++ src/main/java/org/natty/eventsearchers/ics/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The original implemetation of holidays and season was based on ICS files.
+ * This package contains the code how it was more less.
+ * It is currently not used. We may want to resurrect if we enable ICS plugins or so.
+ */
+package org.natty.eventsearchers.ics;
