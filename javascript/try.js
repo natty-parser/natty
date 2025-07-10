7b8412705f6c5f1ff844fca6b987fb766f6eff95
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-10 16:10:40 +0200

    Lets make it work a birt.

--- javascript/try.js
+++ javascript/try.js
@@ -1,13 +1,14 @@
 console.log("Starting...");
-await cheerpjInit()
+await cheerpjInit({ enableDebug: true, version: 17})
+/*
 
 const cj = await cheerpjRunLibrary("/app/natty/natty-1.1.0-SNAPSHOT.jar:/app/natty/antlr-runtime-3.5.3.jar:/app/natty/slf4j-api-2.0.17.jar:/app/natty/slf4j-nop-2.0.17.jar");
+*/
 
-console.log("hoi")
-
+const cj = await cheerpjRunLibrary("/app/slf4j-api-2.0.17.jar:/app/slf4j-nop-2.0.17.jar:/app/antlr-runtime-3.5.3.jar:/app/natty-1.1.0-SNAPSHOT.jar")
 
 
 const Parser= await cj.org.natty.Parser;
-console.log("Parser loaded");
-window.nattyParser = Parser();
+window.nattyParser = await new Parser();
 
+console.log("Parser loaded", window.nattyParser);
