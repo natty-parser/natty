67a9a5b4884fe61575d1345f249a29ea2da08ba7
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2025-07-13 22:25:29 +0200

    Perhaps better to not construct the exception every time..

--- src/main/java/org/natty/eventsearchers/wellknown/WellknownIrregularHoliday.java
+++ src/main/java/org/natty/eventsearchers/wellknown/WellknownIrregularHoliday.java
@@ -4,7 +4,6 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -12,11 +11,15 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 
+import static org.natty.eventsearchers.wellknown.WellknownIrregularHolidaySearcher.SPECIAL_INAUGURATIONS;
+
 /**
   * @since 1.1
   * @author Michiel Meeuwissen
  */
 public enum WellknownIrregularHoliday implements Function<Year, List<LocalDate>>{
+
+
 
   /** First Tuesday after the first Monday in November, US general elections. */
   ELECTION_DAY("US General Election", (year) -> {
@@ -29,57 +32,13 @@ public enum WellknownIrregularHoliday implements Function<Year, List<LocalDate>>
     int y = year.getValue();
     if (y < 1789) return Collections.emptyList();
 
-    // Special/irregular inaugurations (including Washington and Sunday exceptions)
-    List<LocalDate> specialInaugurations = Arrays.asList(
-      LocalDate.of(1789, 4, 30), // Washington
-      LocalDate.of(1793, 3, 4),
-      LocalDate.of(1797, 3, 4),
-      LocalDate.of(1801, 3, 4),
-      LocalDate.of(1805, 3, 4),
-      LocalDate.of(1809, 3, 4),
-      LocalDate.of(1813, 3, 4),
-      LocalDate.of(1817, 3, 4),
-      LocalDate.of(1821, 3, 5),
-      LocalDate.of(1923, 8, 3), // Harding died, Coolidge inaugurated// March 4 was Sunday
-      LocalDate.of(1825, 3, 4),
-      LocalDate.of(1829, 3, 4),
-      LocalDate.of(1833, 3, 4),
-      LocalDate.of(1837, 3, 4),
-      LocalDate.of(1841, 3, 4),
-      LocalDate.of(1845, 3, 4),
-      LocalDate.of(1849, 3, 5),  // March 4 was Sunday
-      LocalDate.of(1850, 7, 9), // Taylor died, Fillmore inaugurated
-      LocalDate.of(1853, 3, 4),
-      LocalDate.of(1857, 3, 4),
-      LocalDate.of(1861, 3, 4),
-      LocalDate.of(1865, 3, 4),
-      LocalDate.of(1865, 4, 15), // Lincoln assassinated, Johnson inaugurated
-      LocalDate.of(1869, 3, 4),
-      LocalDate.of(1873, 3, 4),
-      LocalDate.of(1877, 3, 5),  // March 4 was Sunday
-      LocalDate.of(1881, 3, 4),
-      LocalDate.of(1885, 3, 4),
-      LocalDate.of(1889, 3, 4),
-      LocalDate.of(1893, 3, 4),
-      LocalDate.of(1897, 3, 4),
-      LocalDate.of(1901, 3, 4),
-      LocalDate.of(1905, 3, 4),
-      LocalDate.of(1909, 3, 4),
-      LocalDate.of(1913, 3, 4),
-      LocalDate.of(1917, 3, 5),  // March 4 was Sunday
-      LocalDate.of(1921, 3, 4),
-      LocalDate.of(1925, 3, 4),
-      LocalDate.of(1929, 3, 4),
-      LocalDate.of(1933, 3, 4),
-      LocalDate.of(1963, 11, 22), // Kennedy assassinated, Johnson inaugurated
-      LocalDate.of(1974, 9, 8) // Nixon resigned, Ford inaugurated
 
-    );
-    List<LocalDate> result = new ArrayList<>();
-    for (LocalDate date : specialInaugurations) {
-      if (date.getYear() == y) result.add(date);
+
+    List<LocalDate> d = SPECIAL_INAUGURATIONS.get(y);
+    if (d != null) {
+      return d;
     }
-
+    List<LocalDate> result = new ArrayList<>();
     // Regular inaugurations from 1937 onward (every 4 years)
     if (y >= 1937 && (y - 1937) % 4 == 0) {
       LocalDate jan20 = LocalDate.of(y, 1, 20);
